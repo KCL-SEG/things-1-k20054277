fa06e1b018b55a42c403f2819c0f93e47839643f
Jiaqi Xu <k20054277@kcl.ac.uk>
2022-12-07 20:06:10 +0000

    Refining the Thing model 2

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-	name = models.TextField(UNIQUE=True,blank =False,max_length=30)
-	description = models.TextField(UNIQUE=False,blank =True,max_length=120)
-	quantity = models.IntegerField(UNIQUE=False,validators=[MinValueValidator(0), MaxValueValidator(100)])
+	name = models.TextField(unique=True,blank =False,max_length=30)
+	description = models.TextField(unique=False,blank =True,max_length=120)
+	quantity = models.IntegerField(unique=False,validators=[MinValueValidator(0), MaxValueValidator(100)])
